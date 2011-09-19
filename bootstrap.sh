889a4c92c5040e7650092e7df9c536d77a5dc90e
Glenn Francis Murray <cradle@gmail.com>
2011-09-19 23:28:08 +1000

    fixed reference to xscreensaver file clean up for public

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 su-to-root -X -c /usr/bin/ati_license
 sudo dpkg-reconfigure tzdata
-cp ./xscreensaver ~/
-wget http://ck.kolivas.org/apps/cgminer/cgminer-2.0.3-x86_64-built.tar.bz2
-bunzip2 cgminer*
-tar -xvf cgminer*
+cp .xscreensaver ~/
+#wget http://ck.kolivas.org/apps/cgminer/cgminer-2.0.3-x86_64-built.tar.bz2
+#bunzip2 cgminer*
+#tar -xvf cgminer*
 cd cgminer-2.0.3
 xscreensaver-command -restart
-./cgminer -o http://110.173.227.233:8332/ --intensity 9 --auto-fan --auto-gpu --queue 10 --quiet --text-only --worksize 256
-#iceweasel http://docs.google.com
+#./cgminer -o http://pool.tld:8332/ --intensity 9 --auto-fan --auto-gpu --queue 10 --quiet --text-only --worksize 256
+# xscreensaver-command -activate
+echo "./cgminer <options>"
