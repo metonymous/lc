55dbf85110af2a329afaade3152859ac0a6a5328
Glenn Francis Murray <glenn@metonymous.com>
2011-09-19 12:29:16 +1000

    Updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,6 @@ wget http://ck.kolivas.org/apps/cgminer/cgminer-2.0.3-x86_64-built.tar.bz2
 bunzip2 cgminer*
 tar -xvf cgminer*
 cd cgminer-2.0.3
-#./cgminer -o http://110.173.227.233:8332/ -I 9 -u metonymous.  -p
-ot 
 xscreensaver-command -restart
-iceweasel http://docs.google.com
+./cgminer -o http://110.173.227.233:8332/ --intensity 9 --auto-fan --auto-gpu --queue 10 --quiet --text-only --worksize 256
+#iceweasel http://docs.google.com
