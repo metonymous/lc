180f70fd16008a64d1272386b32a168e5e26931a
Glenn Francis Murray <glenn@metonymous.com>
2011-09-19 01:14:32 +0000

    pull from docs

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+su-to-root -X -c /usr/bin/ati_license
+sudo dpkg-reconfigure tzdata
+xscreensaver
+wget http://ck.kolivas.org/apps/cgminer/cgminer-2.0.3-x86_64-built.tar.bz2
+bunzip2 cgminer*
+tar -xvf cgminer*
+cd cgminer-2.0.3
+#./cgminer -o http://110.173.227.233:8332/ -I 9 -u metonymous.  -p
+ot 
